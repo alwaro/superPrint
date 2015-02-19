a8287be708d8c3cf3cdf017eae246e61c715018b
Alvaro Anaya <alvaro.anaya@gmail.com>
2015-02-19 21:01:53 +0100

    first commit... only a simple structure

---
+++ superPrint.py
@@ -0,0 +1,19 @@
+__author__ = 'alwaro'
+
+
+def printsl(text):
+    """ Print always over the same line """
+    # we need the long of the lines
+    # TODO: look for a compatible way (for all OSes) to get long of lines
+    thwWidth = 80
+
+def main():
+    """
+    main def
+    """
+
+
+
+if __name__ == '__main__':
+    """ Lets do this """
+    main()
